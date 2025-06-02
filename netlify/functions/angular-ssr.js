75abcb6dd4bc8a7395a4703a28db776c26c72b7b
ammarshaffy24 <ammar.20241430@iit.ac.lk>
2025-06-02 16:30:07 +0530

    Fix: Update Angular runtime version and configuration

--- netlify/functions/angular-ssr.js
+++ netlify/functions/angular-ssr.js
@@ -2,5 +2,7 @@ const { createServer } = require('@netlify/angular-runtime');
 
 exports.handler = createServer({
   distPath: 'dist/zamn/browser',
-  indexPath: 'dist/zamn/browser/index.html'
+  indexPath: 'dist/zamn/browser/index.html',
+  enableStaticAssets: true,
+  enableSpaRedirect: true
 }); 
