be09eca3b7fc4914bc09eac2237ada3435cb1069
ammarshaffy24 <ammar.20241430@iit.ac.lk>
2025-06-02 16:07:09 +0530

    Add: Netlify Angular SSR function and configuration

---
+++ netlify/functions/angular-ssr.js
@@ -0,0 +1,6 @@
+const { createServer } = require('@netlify/angular-runtime');
+
+exports.handler = createServer({
+  distPath: 'dist/zamn/browser',
+  indexPath: 'dist/zamn/browser/index.html'
+}); 
